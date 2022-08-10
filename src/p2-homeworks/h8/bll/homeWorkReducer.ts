ba3da9a06bea6c150d7b11b31af54193b6edb490
Vladislav Remnev <remnevvlad30@gmail.com>
2022-08-10 11:21:58 +0300

    HW8 finished

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,37 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: UserType[], action: allTypesReducer) => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            return [...state.sort((a, b) =>
+                action.payload === 'up'
+                    ? b.age - a.age
+                    : a.age - b.age)]
         }
-        case 'check': {
-            // need to fix
-            return state
+        case "check": {
+            return state.filter(user => user.age > action.payload).sort((a, b) => b.age - a.age)
         }
-        default: return state
+        default:
+            return state
     }
+}
+
+type allTypesReducer = sortACType | checkACType
+
+type sortACType = ReturnType<typeof sortAC>
+export const sortAC = (param: string) => {
+    return {
+        type: 'sort',
+        payload: param
+
+    } as const
+}
+
+type checkACType = ReturnType<typeof checkAC>
+export const checkAC = (num: number) => {
+    return {
+        type: 'check',
+        payload: num
+
+    } as const
 }
--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -18,15 +18,18 @@ test('sort name up', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
 
     console.log(newState)
-    // expect(...).toBe(...)
+    expect(newState[0]._id).toBe(1)
+
 })
 test('sort name down', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
 
-
+    console.log(newState)
+    expect(newState[0]._id).toBe(0)
 })
 test('check age 18', () => {
     const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
 
-
+    console.log(newState)
+    expect(newState.length).toBe(4)
 })
